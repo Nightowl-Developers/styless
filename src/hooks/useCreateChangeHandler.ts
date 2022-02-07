08ed9604f554bac3e6e94befb95a0a6edd6bed2d
Joshua Blevins <jblevins@nightowl-developers.com>
2022-02-07 12:09:48 -0500

    feat: event handler creators

---
+++ src/hooks/useCreateChangeHandler.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const useCreateBlurHandler = <ElementType>(
+    onChange: (event: React.ChangeEvent<ElementType>) => void,
+    disabled: boolean
+) => {
+    return React.useCallback((event: React.ChangeEvent<ElementType>) => {
+        if (!disabled) {
+            if (onChange) {
+                onChange(event);
+            }
+        }
+    }, [
+        disabled,
+        onChange
+    ]); 
+};
+
+export default useCreateBlurHandler;
---
+++ src/hooks/useCreateFocusHandler.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const useCreateFocusHandler = <ElementType>(
+    onFocus: (event: React.FocusEvent<ElementType>) => void,
+    disabled: boolean
+) => {
+    return React.useCallback((event: React.FocusEvent<ElementType>) => {
+        if (!disabled) {
+            if (onFocus) {
+                onFocus(event);
+            }
+        }
+    }, [
+        disabled,
+        onFocus
+    ]);     
+};
+
+export default useCreateFocusHandler;
